88f2b3c219f2afac2ed129a56f813c120f598109
Ose-Augustine <osebeckley@gmail.com>
2023-03-31 17:08:04 +0100

    Configure devise to show users show page after sign in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+    #Configures devise to redirect to 
+    #a user's show page after login
+    def after_sign_in_path_for(resource)
+        user_path(resource) 
+    end 
 end
