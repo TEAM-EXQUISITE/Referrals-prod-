029226247874fb113456ed245fa66fd3909d3187
Ose-Augustine <osebeckley@gmail.com>
2023-04-01 07:09:20 +0100

    Test for user signup

--- db/migrate/20230331032057_add_users_id__to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddUsersIdToEvents < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :events, :users, null: false, foreign_key: true
-  end
-end
---
+++ test/integration/user_signup_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+  def setup 
+    @user = users(:michael)
+  end
+
+  test "User can sign up " do 
+    get new_user_path 
+    assert_template 'users/new' 
+    post users_path , params: {
+      user: {
+        name: 'janet', 
+        email:'janet@example.com', 
+        password:'password',
+        password_confirmation: 'password'
+      }
+    }
+    assert_response :success
+  end 
+end
