e1a80b46354cfa23ed0be95c214e8d4031645de2
Ose-Augustine <osebeckley@gmail.com>
2023-04-01 08:36:52 +0100

    More signup integration tests.

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -16,7 +16,37 @@ class UserSignupTest < ActionDispatch::IntegrationTest
         password_confirmation: 'password'
       }
     }
+    assert_not flash.empty?
     follow_redirect! 
     assert_template 'users/show' 
+    assert flash.empty?
   end 
+
+  test "Should render error partial with password length less than six" do 
+    get new_user_path 
+    assert_template 'users/new' 
+    post users_path , params: {
+      user: {
+        name: @user.name, 
+        email: @user.email, 
+        password: 'pass', 
+        password_confirmation: 'pass', 
+      }
+    }
+    assert_template partial: '_error_messages'
+  end 
+
+  test "Should render error for wrong email format" do 
+    get new_user_path 
+    post users_path , params: {
+      user: {
+        name: @user.name, 
+        email: 'wrong', 
+        password:'password', 
+        password_confirmation: 'password',
+      }
+    }
+    assert_template partial: '_error_messages' 
+    assert_template 'users/new' 
+  end
 end
