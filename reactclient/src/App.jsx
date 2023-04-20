d33967771ac29391e65ebf1e69d6c3dc41d3e544
Ose-Augustine <osebeckley@gmail.com>
2023-04-20 17:16:53 +0100

    Making the getAPIData function resuseable.

--- reactclient/src/App.jsx
+++ reactclient/src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
 import UsersIndex from './components/users/UsersIndex'
+import getAPIData from '../api-connection'
+import { useEffect, useState } from 'react';
+
+const API_URL = 'https://localhost:5000/users' 
+
 function App() {
+  const [users, setUsers] = useState([]); 
+
+  useEffect(() => {
+    let mounted = true; 
+    getAPIData(API_URL).then((items) => {
+      if(mounted) {
+        setUsers(items); 
+      }
+    })
+    return () => {mounted = false;}; 
+  },[]);
   return (
    <>
     <div>hello</div>
-    <UsersIndex />
+    <UsersIndex users = {users}/>
    </>
   )
 }
--- reactclient/src/components/users/UsersIndex.jsx
+++ reactclient/src/components/users/UsersIndex.jsx
@@ -1,27 +1,16 @@
-import axios from 'axios'; 
-import { useEffect, useState } from 'react';
 
-const API_URL = "https://localhost:5000/users"; 
-
-//gets api data
-async function getAPIData(){
-    const response = await axios.get(API_URL); 
-    return response.data; 
-}
-export default function Index(){
-    const [users, setUsers] = useState([]); 
-
-    useEffect (() =>{
-        let mounted = true; 
-        getAPIData().then((items) => {
-            if(mounted){
-                setUsers(items); 
-            }
-        })
-        return () => {mounted = false}; 
-    }, []);
-
+export default function UsersIndex(props){
     return (
-        <div>This is index</div>
+        <div>
+            <h1>These users are from the api</h1>
+            {props.users.map((user) => {
+                return (
+                    <div key={user.id}>
+                        <h2>{user.name}</h2>
+                        <p>{user.email}</p>
+                    </div>
+                )
+            })} 
+        </div>
     );
 }
