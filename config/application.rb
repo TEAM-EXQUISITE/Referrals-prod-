e0a7866b4051f3e38990c6b62f5547adb26dfc07
Ose-Augustine <osebeckley@gmail.com>
2023-04-20 12:39:01 +0100

    Handling CORs.

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,11 @@ module PrivateEvents
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do 
+      allow do 
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :patch, :options] 
+      end
+    end
   end
 end
---
+++ users/src/App.jsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>This is a new react app</div>
+  )
+}
+
+export default App
